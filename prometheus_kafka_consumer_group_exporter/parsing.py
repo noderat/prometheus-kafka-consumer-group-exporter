58f64a7fbd52c9802e9b274744b558c954530708
Jake Utley <jutley@hiya.com>
2019-03-04 17:04:11 -0800

    Add support for Kafka 2.1
    Signed-off-by: Chris Coggburn <noderat@gmail.com>

--- prometheus_kafka_consumer_group_exporter/parsing.py
+++ prometheus_kafka_consumer_group_exporter/parsing.py
@@ -1,5 +1,5 @@
 from struct import unpack_from, error as struct_error
-
+import logging
 
 def read_short(bytes):
     num = unpack_from('>h', bytes)[0]
@@ -30,27 +30,45 @@ def parse_key(bytes):
     try:
         (version, remaining_key) = read_short(bytes)
         if version == 1 or version == 0:
+            # https://github.com/apache/kafka/blob/2.1.1/core/src/main/scala/kafka/coordinator/group/GroupMetadataManager.scala#L936-L938
             (group, remaining_key) = read_string(remaining_key)
             (topic, remaining_key) = read_string(remaining_key)
             (partition, remaining_key) = read_int(remaining_key)
             return (version, group, topic, partition)
     except struct_error:
-        pass
+        logging.warn("Failed to parse key")
 
 
 def parse_value(bytes):
     try:
         (version, remaining_key) = read_short(bytes)
         if version == 0:
+            # https://github.com/apache/kafka/blob/2.1.1/core/src/main/scala/kafka/coordinator/group/GroupMetadataManager.scala#L943-L945
             (offset, remaining_key) = read_long_long(remaining_key)
             (metadata, remaining_key) = read_string(remaining_key)
             (timestamp, remaining_key) = read_long_long(remaining_key)
             return (version, offset, metadata, timestamp)
         elif version == 1:
+            # https://github.com/apache/kafka/blob/2.1.1/core/src/main/scala/kafka/coordinator/group/GroupMetadataManager.scala#L950-L953
             (offset, remaining_key) = read_long_long(remaining_key)
             (metadata, remaining_key) = read_string(remaining_key)
             (commit_timestamp, remaining_key) = read_long_long(remaining_key)
             (expire_timestamp, remaining_key) = read_long_long(remaining_key)
             return (version, offset, metadata, commit_timestamp, expire_timestamp)
+        elif version == 2:
+            # https://github.com/apache/kafka/blob/2.1.1/core/src/main/scala/kafka/coordinator/group/GroupMetadataManager.scala#L959-L961
+            (offset, remaining_key) = read_long_long(remaining_key)
+            (metadata, remaining_key) = read_string(remaining_key)
+            (commit_timestamp, remaining_key) = read_long_long(remaining_key)
+            return (version, offset, metadata, commit_timestamp)
+        elif version == 3:
+            # https://github.com/apache/kafka/blob/2.1.1/core/src/main/scala/kafka/coordinator/group/GroupMetadataManager.scala#L966-L970
+            (offset, remaining_key) = read_long_long(remaining_key)
+            (leader_epoch, remaining_key) = read_int(remaining_key)
+            (metadata, remaining_key) = read_string(remaining_key)
+            (commit_timestamp, remaining_key) = read_long_long(remaining_key)
+            return (version, offset, metadata, commit_timestamp)
+        else:
+            logging.warn("Did not recognize value schema version")
     except struct_error:
-        pass
+        logging.warn("Failed to parse value")
